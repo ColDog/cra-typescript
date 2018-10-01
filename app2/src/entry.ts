30445b8b4e9b54dc70a6d4c7c6430d8231b5557f
colin <colin@coldog.mint>
2018-10-01 15:52:01 -0700

    Try basic app.

---
+++ app2/src/entry.ts
@@ -0,0 +1,7 @@
+import * as lib from '@app/lib';
+
+interface App {}
+
+export const func = (app: App): string => {
+  return lib.Hello
+}
---
+++ app2/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: {
+    app: ["./src/entry.ts"],
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      // babel-loader for pure javascript (es6) => javascript (es5)
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        loader: require.resolve('babel-loader'),
+        exclude: []
+      }
+    ]
+  },
+  plugins: [],
+  resolve: {
+    modules: ['node_modules', 'src'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  }
+};
